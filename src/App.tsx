3f6e2e9082e42263a944cd3fc706f7b6357c18c7
Amir Hossein Mafi <amir77mafi@gmail.com>
2023-07-25 12:36:32 +0330

    add styling for test page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { ChangeEventHandler, useCallback, useState } from "react";
 import "./App.css";
 import Dialog from "./Dialog";
+import styles from "./test.module.css";
 
 function App() {
     const [type, setType] = useState("");
@@ -18,13 +19,21 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <span>نوع:</span>
-            <input value={type} onChange={typeChange} />
-            <button onClick={showAlert}>پیغام</button>
-            <button>تأیید</button>
+        <div className={styles.container}>
+            <span className={styles.label}>نوع:</span>
+            <input value={type} onChange={typeChange} className={styles.input} />
+            <div className={styles.buttonsWrapper}>
+                <button onClick={showAlert} className={`${styles.button} ${styles.messageBtn}`}>
+                    پیغام
+                </button>
+                <button className={`${styles.button} ${styles.submitBtn}`}>تأیید</button>
+            </div>
 
-            <div id="output" style={{ padding: "1em", border: "1px solid black" }}>
+            <div
+                id="output"
+                style={{ padding: "1em", border: "1px solid black" }}
+                className={styles.output}
+            >
                 خروجی:
             </div>
 
